589ad58d49f10752d8190aadadb6f49d6cf90ff3
stevebstorm <steve.lorent@gmail.Com>
2024-05-08 09:29:43 +0200

    css

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -9,7 +9,17 @@ import { Link } from '../../models/link.model';
 export class NavbarComponent {
   listeLien : Link[] = [
     {url : 'demo', name : 'Les démos', children : [
-      {url : 'demo1', name : 'Typescript'},
+      {url : 'demo/demo1', name : 'Typescript'},
+      {url : 'demo/demo2', name : 'Bindings'},
+      {url : 'demo/demo3', name : 'Pipes'},
+      {url : 'demo/demo4', name : 'Directives'},
     ]},
+    {url : 'exo', name : 'Les exos', children : [
+      {url : 'exo/exo1', name : 'Chronomètre'}
+    ]}
   ]
+
+  showChildren(index : number){
+    this.listeLien[index].isChildrenVisible = !this.listeLien[index].isChildrenVisible
+  }
 }
